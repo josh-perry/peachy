40b363e3b25bbc77d037faace6cb0f2b911e8a02
Brandon Blanker Lim-it <flamendless8@gmail.com>
2018-07-19 08:58:38 +0800

    updated docs and put ox and oy

--- peachy.lua
+++ peachy.lua
@@ -133,7 +133,9 @@ end
 -- @tparam number rot the rotation to draw at.
 -- @tparam number sx the x scaling.
 -- @tparam number sy the y scaling.
-function peachy:draw(x, y, rot, sx, sy)
+-- @tparam number ox the x origin offset.
+-- @tparam number oy the y origin scaling.
+function peachy:draw(x, y, rot, sx, sy, ox , oy)
   if not self.frame then
     return
   end
